ccfde3981847369a4ae4b9ae6328fe6aa12dec7f
Talitha Brown <talithabrown128@gmail.com>
2022-01-20 15:32:35 -0700

    week 3 code exercise

--- js/main.js
+++ js/main.js
@@ -6,6 +6,18 @@ const links = [
     {
       label: "Week 2 code example",
       url: "week2/index.html"
+    },
+    {
+      label: "Week 2 team assignment",
+      url: "week2/team-assignment/index.html"
+    },
+    {
+      label: "Week 3 code example",
+      url: "week3/index.html"
+    },
+    {
+      label: "Week 3 team assignment",
+      url: "week3/array-cardio-day-1.html"
     }
 ]
 
